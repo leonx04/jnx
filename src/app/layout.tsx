54785d12b29953f4e3102e5f195effb251d8da1b
leonx04 <jungnguyen2108@gmail.com>
2024-12-21 21:10:22 +0700

    Update metadata for improved SEO and social sharing representation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,25 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "JNX Tennis Store",
-  description: "JNX Tennis Store is a tennis store that sells tennis equipment and accessories.",
+  description: "JNX Tennis Store is a tennis store that sells high-quality tennis equipment and accessories for all levels of play.",
+  openGraph: {
+    title: 'JNX Tennis Store - High-Quality Tennis Rackets',
+    description: 'JNX Tennis Store is a service that provides high-quality tennis rackets and equipment for all levels of play. We offer a wide range of professional and beginner rackets to suit your needs.',
+    images: ['https://jnx-store.netlify.app/_next/image?url=https%3A%2F%2Fscontent.fhan17-1.fna.fbcdn.net%2Fv%2Ft39.30808-6%2F471300448_986822023488080_7111729637380421990_n.jpg%3F_nc_cat%3D108%26ccb%3D1-7%26_nc_sid%3D6ee11a%26_nc_ohc%3DP4npL3jSWGQQ7kNvgEvlUfJ%26_nc_oc%3DAdgQECd9WWURtcIipQfoK1uNQr1RylKPiqpNUFwxSsIwcNtLZBLEqhRHF0hZeAtyFBA%26_nc_zt%3D23%26_nc_ht%3Dscontent.fhan17-1.fna%26_nc_gid%3DAQO-Zwp16Nzr_U2Y8hanAM5%26oh%3D00_AYD94Csm4v8fS7VhgErsYdglt1oMKFda45uG7bQ-8NnMWA%26oe%3D676C2FC9&w=1920&q=75'],
+    url: 'https://jnx-store.netlify.app',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'JNX Tennis Store - High-Quality Tennis Rackets',
+    description: 'JNX Tennis Store is a service that provides high-quality tennis rackets and equipment for all levels of play. We offer a wide range of professional and beginner rackets to suit your needs.',
+    images: ['https://jnx-store.netlify.app/_next/image?url=https%3A%2F%2Fscontent.fhan17-1.fna.fbcdn.net%2Fv%2Ft39.30808-6%2F471300448_986822023488080_7111729637380421990_n.jpg%3F_nc_cat%3D108%26ccb%3D1-7%26_nc_sid%3D6ee11a%26_nc_ohc%3DP4npL3jSWGQQ7kNvgEvlUfJ%26_nc_oc%3DAdgQECd9WWURtcIipQfoK1uNQr1RylKPiqpNUFwxSsIwcNtLZBLEqhRHF0hZeAtyFBA%26_nc_zt%3D23%26_nc_ht%3Dscontent.fhan17-1.fna%26_nc_gid%3DAQO-Zwp16Nzr_U2Y8hanAM5%26oh%3D00_AYD94Csm4v8fS7VhgErsYdglt1oMKFda45uG7bQ-8NnMWA%26oe%3D676C2FC9&w=1920&q=75'],
+  },
+  keywords: ['tennis rackets', 'tennis equipment', 'professional rackets', 'beginner rackets'],
+  authors: [{ name: 'JNX Tennis Store' }],
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -36,8 +54,8 @@ export default function RootLayout({
             {children}
           </main>
           <Footer />
-          <Toaster 
-            position="top-right" 
+          <Toaster
+            position="top-right"
             toastOptions={{
               className: 'toast-below-navbar',
               duration: 2000,
