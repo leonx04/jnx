a4a5cbfd10fd65eef598ee17758e79117d444478
leonx04 <jungnguyen2108@gmail.com>
2024-12-21 21:12:49 +0700

    Refactor import statement for consistency in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import type { Metadata } from "next";
+import { Metadata } from 'next'
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
 import Footer from "./components/Footer";
