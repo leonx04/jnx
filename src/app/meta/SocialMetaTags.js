5d2603a2e9cadd30dd06e7acf9b04b2d83a09bac
leonx04 <jungnguyen2108@gmail.com>
2024-12-21 15:22:31 +0700

    Update social meta tags image URL for improved accessibility and performance

--- src/app/meta/SocialMetaTags.js
+++ src/app/meta/SocialMetaTags.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const SocialMetaTags = ({
   title = 'JNX Tennis Store - High-Quality Tennis Rackets',
   description = ' JNX Tennis Store is a service that provides high-quality tennis rackets and equipment for all levels of play. We offer a wide range of professional and beginner rackets to suit your needs.',
-  image = 'https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/471300448_986822023488080_7111729637380421990_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=P4npL3jSWGQQ7kNvgEvlUfJ&_nc_oc=AdgQECd9WWURtcIipQfoK1uNQr1RylKPiqpNUFwxSsIwcNtLZBLEqhRHF0hZeAtyFBA&_nc_zt=23&_nc_ht=scontent.fhan17-1.fna&_nc_gid=AQO-Zwp16Nzr_U2Y8hanAM5&oh=00_AYD94Csm4v8fS7VhgErsYdglt1oMKFda45uG7bQ-8NnMWA&oe=676C2FC9',
+  image = 'https://jnx-store.netlify.app/_next/image?url=https%3A%2F%2Fscontent.fhan17-1.fna.fbcdn.net%2Fv%2Ft39.30808-6%2F471300448_986822023488080_7111729637380421990_n.jpg%3F_nc_cat%3D108%26ccb%3D1-7%26_nc_sid%3D6ee11a%26_nc_ohc%3DP4npL3jSWGQQ7kNvgEvlUfJ%26_nc_oc%3DAdgQECd9WWURtcIipQfoK1uNQr1RylKPiqpNUFwxSsIwcNtLZBLEqhRHF0hZeAtyFBA%26_nc_zt%3D23%26_nc_ht%3Dscontent.fhan17-1.fna%26_nc_gid%3DAQO-Zwp16Nzr_U2Y8hanAM5%26oh%3D00_AYD94Csm4v8fS7VhgErsYdglt1oMKFda45uG7bQ-8NnMWA%26oe%3D676C2FC9&w=1920&q=75',
   url = 'https://jnx-store.netlify.app',
   type = 'JNX Tennis Store'
 }) => {
