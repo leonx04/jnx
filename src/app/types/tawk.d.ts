3e09202ed4e897c97bac2263d0417ba088c007cf
leonx04 <jungnguyen2108@gmail.com>
2024-12-23 14:11:35 +0700

    Add TypeScript definitions for Tawk_API and Tawk_LoadStart in global Window interface

--- src/app/types/tawk.d.ts
+++ src/app/types/tawk.d.ts
@@ -1,5 +1,7 @@
 declare global {
     interface Window {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        // eslint-disable-next-line
         Tawk_API?: any;
         Tawk_LoadStart?: Date;
     }
